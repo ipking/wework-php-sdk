3e6cae058b1e61434e28622c6e3371558c62d220
ipking <667997@gmail.com>
2018-12-05 17:24:13 +0800

    fix autoload

--- autoload.php
+++ autoload.php
@@ -5,9 +5,10 @@ spl_autoload_register(function($class_name){
 
 	//WeWork
 	if(preg_match('/^WeWork/', $class_name)){
-		$path = str_replace('WeWork', 'WeWork\src', $path);
-		$path = str_replace('\\', $ds, trim($class_name, '\\'));
-		$file = str_replace('WeWork','',__DIR__).$path.'.php';
+		$path = str_replace('WeWork', 'WeWork\src', $class_name);
+		$path = str_replace('\\', $ds, trim($path, '\\'));
+		$path = str_replace('WeWork', '', trim($path, '\\'));
+		$file = __DIR__.$path.'.php';
 		if(is_file($file)){
 			include_once $file;
 		}
